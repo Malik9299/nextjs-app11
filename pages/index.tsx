00b04472cc5fb717070fc69e432c173bca2d1315
Malik-Abdul <abdul.ghafoor@dubizzlelabs.com>
2023-10-10 18:28:58 +0500

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import { Landing } from "@/templates";
+
+export default function LandingPage() {
+  return <Landing />;
+}
---
+++ src/components/templates/Landing/index.tsx
@@ -0,0 +1,7 @@
+import { Fragment } from "react";
+
+const Landing = () => {
+  return <Fragment>Here is Landing page</Fragment>;
+};
+
+export default Landing;
